6b49207a4e4b203c60aaab742be94f1a0a479989
dilipkumarbkv <85829232+dilipkumarbkv@users.noreply.github.com>
2021-06-20 15:11:50 -0300

    Add files via upload
    Functions assignment

---
+++ day6.py
@@ -0,0 +1,33 @@
+def myfunc():
+    x = int(input("Enter the 1st number:"))
+    y = int(input("Enter the 2nd number:"))
+    z = x + y
+    print("Addition of two numbers:", z)
+    z = x - y
+    print("Subtraction of two numbers:", z)
+    z = x * y
+    print("Multiplication of two numbers:", z)
+    z = x // y
+    print("Division of two numbers=", z)
+
+
+myfunc()
+
+
+
+# Covid fuction
+
+x = input("Enter the  C.patient name:")
+x = 98
+if (x!=98):
+    print("enter the body temperature:", numb)
+else:
+    print("the body temp is=98")
+
+x = input("enter the patient name:")
+
+
+def covid():
+    print("The patient body tempature is:98")
+
+covid()
