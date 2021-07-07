f20291b40f3a91e22a4b858d4a69290b6103c63d
dilipkumarbkv <85829232+dilipkumarbkv@users.noreply.github.com>
2021-07-07 08:18:20 -0300

    Add files via upload
    PIP and Modules

---
+++ day10 pandas.py
@@ -0,0 +1,15 @@
+# Install Pandas
+
+# OPen Windows Terminal and type the command "pip install Pandas"
+
+
+import pandas as pd
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+
+myvar = pd.DataFrame(mydataset)
+
+print(myvar)
---
+++ day10.py
@@ -0,0 +1,3 @@
+# Install Pandas
+
+pip install Pandas
